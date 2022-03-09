58cad36f1abe81ad29f917c64892ca8513329578
C0821G1-DangHuuDu <huud90@gmail.com>
2022-03-09 16:03:23 +0700

    up git 9-3

--- c0821g1-building-management/src/app/feature/space/space-edit/space-edit.component.ts
+++ c0821g1-building-management/src/app/feature/space/space-edit/space-edit.component.ts
@@ -16,67 +16,60 @@ import {Floors} from "../../../model/floor/floors";
   styleUrls: ['./space-edit.component.css']
 })
 export class SpaceEditComponent implements OnInit {
-  spaceEditForm: FormGroup;
+  spaceEditForm = new FormGroup({
+    spaceCode: new FormControl('', Validators.required),
+    spaceArea: new FormControl('', [Validators.required, Validators.pattern('^(,|[0-9])*$')]),
+    spacePrice: new FormControl('', [Validators.pattern('^(,|[0-9])*$')]),
+    spaceManagerFee: new FormControl('', [Validators.pattern('^(,|[0-9])*$')]),
+    spaceNote: new FormControl(),
+    spaceImage: new FormControl(),
+    spaceDeleteFlag: new FormControl(),
+    spacesType: new FormControl('', Validators.required),
+    spaceStatus: new FormControl('', Validators.required),
+    floors: new FormControl('', Validators.required)
+  });
   spaceEdit: Spaces;
   spaceTypeList: Array<SpacesType>;
   spaceStatusList: Array<SpacesStatus>;
   floorList: Array<Floors>;
+
   constructor(private spaceService: SpaceService,
               private spaceTypeService: SpaceTypeService,
               private spaceStatusService: SpaceStatusService,
               private floorService: FloorService,
               private activatedRoute: ActivatedRoute,
               private router: Router) {
-    this.spaceEditForm = new FormGroup({
-      spaceCode: new FormControl('', Validators.required),
-      spaceArea: new FormControl('', Validators.required),
-      spacePrice: new FormControl('', Validators.pattern('^(\.|[0-9])*$')),
-      spaceManagerFee: new FormControl('', Validators.pattern('^(\.|[0-9])*$')),
-      spaceNote: new FormControl(),
-      spaceImage: new FormControl(),
-      spaceDeleteFlag: new FormControl(),
-      spacesType: new FormControl('', Validators.required),
-      spaceStatus: new FormControl('', Validators.required),
-      floors: new FormControl('', Validators.required)
-    });
-    this.spaceStatusService.findAll().subscribe(value => {
-      this.spaceStatusList = value;
-    });
-    this.spaceTypeService.findAll().subscribe(value => {
-      this.spaceTypeList = value;
-    });
-    this.floorService.findAll().subscribe(value => {
-      this.floorList = value;
-    });
   }
 
   ngOnInit(): void {
-    const spaceEditId = this.activatedRoute.snapshot.params.id;
-    this.spaceService.findByID(spaceEditId).subscribe(value => {
-      this.spaceEdit = value;
-      console.log(this.spaceEdit);
-      this.spaceEditForm.patchValue({
-        spaceCode: this.spaceEdit.spaceCode,
-        spaceArea: this.spaceEdit.spaceArea,
-        spacePrice: this.spaceEdit.spacePrice,
-        spaceManagerFee: this.spaceEdit.spaceManagerFee,
-        spaceNote: this.spaceEdit.spaceNote,
-        spaceImage: this.spaceEdit.spaceImage,
-        spaceDeleteFlag: this.spaceEdit.spaceDeleteFlag,
-        spacesType: this.spaceEdit.spacesType.spaceTypeName,
-        spaceStatus: this.spaceEdit.spaceStatus.spacerStatusName,
-        floors: this.spaceEdit.floors.floorName,
+    this.spaceStatusService.findAll().subscribe(value => {
+      this.spaceStatusList = value;
+      this.spaceTypeService.findAll().subscribe(value1 => {
+        this.spaceTypeList = value1;
+        this.floorService.findAll().subscribe(value2 => {
+          this.floorList = value2;
+          const spaceEditId = this.activatedRoute.snapshot.params.id;
+          this.spaceService.findByID(spaceEditId).subscribe(value3 => {
+            this.spaceEdit = value3;
+              // console.log(this.spaceEdit);
+            this.spaceEditForm.patchValue(this.spaceEdit);
+          });
+        });
       });
     });
-    console.log(spaceEditId);
+
+
 
   }
-  editSpace(): void{
+
+  editSpace(): void {
     const editSpace = Object.assign({}, this.spaceEditForm.value);
     editSpace.spaceId = this.spaceEdit.spaceId;
-    this.spaceService.editSpace(editSpace).subscribe(value => { },
-      error => {}, () => {
-          this.router.navigateByUrl('/space/list');
+    this.spaceService.editSpace(editSpace).subscribe(value => {alert('Chỉnh sửa thành công');
+      },
+      error => {
+      }, () => {
+        this.router.navigateByUrl('/space/list');
       });
   }
 }
