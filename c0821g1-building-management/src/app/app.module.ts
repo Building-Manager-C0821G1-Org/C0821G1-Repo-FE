34ffa0547fab253783a35a3c570300b42aaedd41
Duy Nguyen <you@example.com>
2022-03-10 23:19:35 +0700

    DuyNP push floor-list 11:19PM

--- c0821g1-building-management/src/app/app.module.ts
+++ c0821g1-building-management/src/app/app.module.ts
@@ -1,38 +1,34 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {CustomerModule} from './feature/customer/customer.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {BodyComponent} from './shared/body/body.component';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
 import {FloorModule} from './feature/floors/floor.module';
+import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-
-
-
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-    HttpClientModule,
-    ReactiveFormsModule,
-    CustomerModule,
-    BrowserAnimationsModule,
     FloorModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     MatDialogModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
