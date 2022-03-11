c384572759393f7ae7d2442e2c39f084b25100e7
Duy Nguyen <you@example.com>
2022-03-11 09:35:55 +0700

    up

--- c0821g1-building-management/src/app/app-routing.module.ts
+++ c0821g1-building-management/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'customer',
+    loadChildren: () => import('./feature/customer/customer.module').then(module => module.CustomerModule)
+  },
+  {
     path: 'floors',
     loadChildren: () => import('./feature/floors/floor.module.js').then(module => module.FloorModule)
   },
