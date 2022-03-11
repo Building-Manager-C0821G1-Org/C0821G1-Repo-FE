725eceaf1d5bd5bacda60ba9af06139cd8e6d1b6
Duy Nguyen <you@example.com>
2022-03-11 09:55:57 +0700

    up

--- c0821g1-building-management/src/app/feature/customer/customer-routing.module.ts
+++ c0821g1-building-management/src/app/feature/customer/customer-routing.module.ts
@@ -1,21 +1,11 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {CustomerListComponent} from './customer-list/customer-list.component';
-import {DeleteCustomerComponent} from "./delete-customer/delete-customer.component";
-import {CustomerCreateComponent} from "./customer-create/customer-create.component";
-import {CustomerEditComponent} from "./customer-edit/customer-edit.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [
-  {path: 'list', component: CustomerListComponent},
-  {path: 'delete/:id', component: DeleteCustomerComponent},
-  {path: 'create', component: CustomerCreateComponent},
-  {path: 'edit/:id', component: CustomerEditComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CustomerRoutingModule {
-}
+export class CustomerRoutingModule { }
