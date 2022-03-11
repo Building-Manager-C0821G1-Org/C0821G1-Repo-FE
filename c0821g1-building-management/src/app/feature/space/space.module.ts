0ed28e3c06b6f99cc320828551327dd154b8f02b
Duy Nguyen <you@example.com>
2022-03-11 22:38:48 +0700

    up

--- c0821g1-building-management/src/app/feature/space/space.module.ts
+++ c0821g1-building-management/src/app/feature/space/space.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SpaceRoutingModule } from './space-routing.module';
-import { SpaceListComponent } from './space-list/space-list.component';
-import { SpaceCreateComponent } from './space-create/space-create.component';
-import { SpaceEditComponent } from './space-edit/space-edit.component';
+// import { SpaceListComponent } from './space-list/space-list.component';
+// import { SpaceCreateComponent } from './space-create/space-create.component';
+// import { SpaceEditComponent } from './space-edit/space-edit.component';
 
 
 @NgModule({
-  declarations: [SpaceListComponent, SpaceCreateComponent, SpaceEditComponent],
+  // declarations: [SpaceListComponent, SpaceCreateComponent, SpaceEditComponent],
   imports: [
     CommonModule,
     SpaceRoutingModule
